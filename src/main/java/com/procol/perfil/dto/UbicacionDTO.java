28780804836f639a773beb0b74a73651a05a147a
RAAR-18 <raacuna@unimagdalena.edu.co>
2025-11-14 18:16:34 -0500

    feat: agregar UbicacionDTO

---
+++ src/main/java/com/procol/perfil/dto/UbicacionDTO.java
@@ -0,0 +1,87 @@
+package com.procol.perfil.dto;
+
+public class UbicacionDTO {
+
+    private Integer idUbicacion;
+    private Integer idPadreUbicacion;
+    private String nombreUbicacion;
+    private String idDaneUbicacion;
+    private String longitudUbicacion;
+    private String latitudUbicacion;
+
+    public UbicacionDTO() {
+    }
+
+    public UbicacionDTO(
+            Integer idUbicacion, Integer idPadreUbicacion, String nombreUbicacion,
+            String idDaneUbicacion, String longitudUbicacion, String latitudUbicacion
+    ) {
+        this.idUbicacion = idUbicacion;
+        this.idPadreUbicacion = idPadreUbicacion;
+        this.nombreUbicacion = nombreUbicacion;
+        this.idDaneUbicacion = idDaneUbicacion;
+        this.longitudUbicacion = longitudUbicacion;
+        this.latitudUbicacion = latitudUbicacion;
+    }
+
+    public Integer getIdUbicacion() {
+        return idUbicacion;
+    }
+
+    public void setIdUbicacion(Integer idUbicacion) {
+        this.idUbicacion = idUbicacion;
+    }
+
+    public Integer getIdPadreUbicacion() {
+        return idPadreUbicacion;
+    }
+
+    public void setIdPadreUbicacion(Integer idPadreUbicacion) {
+        this.idPadreUbicacion = idPadreUbicacion;
+    }
+
+    public String getNombreUbicacion() {
+        return nombreUbicacion;
+    }
+
+    public void setNombreUbicacion(String nombreUbicacion) {
+        this.nombreUbicacion = nombreUbicacion;
+    }
+
+    public String getIdDaneUbicacion() {
+        return idDaneUbicacion;
+    }
+
+    public void setIdDaneUbicacion(String idDaneUbicacion) {
+        this.idDaneUbicacion = idDaneUbicacion;
+    }
+
+    public String getLongitudUbicacion() {
+        return longitudUbicacion;
+    }
+
+    public void setLongitudUbicacion(String longitudUbicacion) {
+        this.longitudUbicacion = longitudUbicacion;
+    }
+
+    public String getLatitudUbicacion() {
+        return latitudUbicacion;
+    }
+
+    public void setLatitudUbicacion(String latitudUbicacion) {
+        this.latitudUbicacion = latitudUbicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "UbicacionDTO["
+                + "idUbicacion=" + idUbicacion
+                + ", idPadreUbicacion=" + idPadreUbicacion
+                + ", nombreUbicacion=" + nombreUbicacion
+                + ", idDaneUbicacion=" + idDaneUbicacion
+                + ", longitudUbicacion=" + longitudUbicacion
+                + ", latitudUbicacion=" + latitudUbicacion
+                + "]";
+    }
+
+}
